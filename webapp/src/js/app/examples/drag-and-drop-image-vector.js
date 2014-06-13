34b88a2d7d628f8ad8f20661a9f8a4792e8671b7
peturh <petur.hjartarson@cybercom.com>
2014-06-13 13:29:50 +0200

    added some info to drag and drop

--- webapp/src/js/app/examples/drag-and-drop-image-vector.js
+++ webapp/src/js/app/examples/drag-and-drop-image-vector.js
@@ -3,6 +3,8 @@
  * Build this into angular!?
  *
  * Try it out: http://ol3js.org/en/master/examples/drag-and-drop-image-vector.html
+ * There is a .geojson file inside assets that you can drag and drop into the map
+ *
  */
 
 var defaultStyle = {
