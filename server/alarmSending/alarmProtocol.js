48ee44b73d599be849fff1cf3a79b39fe895d893
Stefan Blixt <Stefan.Blixt@cybercom.com>
2014-06-17 19:27:28 +0200

    Emitting!

--- server/alarmSending/alarmProtocol.js
+++ server/alarmSending/alarmProtocol.js
@@ -2,7 +2,7 @@
 function sendAlarm(event, coordinates, socket) {
     var buffer = event + "/" + coordinates.latitude + "/" + coordinates.longitude;
     console.log("Connected, sending: " + buffer);
-    socket.broadcast.emit('signal', buffer);
+    socket.emit('signal', buffer);
 }
 
 
