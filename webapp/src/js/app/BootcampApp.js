14e00ec4a72f65a78bc8dfc4912a1094714b9f11
peturh <petur.hjartarson@cybercom.com>
2014-06-17 18:14:50 +0200

    added more socket files

--- webapp/src/js/app/BootcampApp.js
+++ webapp/src/js/app/BootcampApp.js
@@ -1,1 +1,1 @@
-var app = angular.module("app", ['btford.socket-io']);
+var app = angular.module("app", ['btford.socket-io', 'chatSocket']);
---
+++ webapp/src/js/app/controllers/SocketCtrl.js
@@ -0,0 +1,11 @@
+/**
+ * Created by petur on 2014-06-17.
+ */
+
+app.controller('SocketCtrl',['$scope', 'chatSocket', function($scope, chatSocket){
+
+        $scope.operator = operatorName;
+        $scope.messageLog = 'Ready to receive signals!';
+
+
+}]);
